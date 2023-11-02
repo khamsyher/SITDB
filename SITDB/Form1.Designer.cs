9524630a09c4570d0a4092d4e721cb0ddab049dc
khamsyher <114057321+khamsyher@users.noreply.github.com>
2023-11-02 18:22:09 +0700

    Add project files.

---
+++ SITDB/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SITDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cnbt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cnbt
+            // 
+            this.cnbt.Location = new System.Drawing.Point(43, 34);
+            this.cnbt.Name = "cnbt";
+            this.cnbt.Size = new System.Drawing.Size(204, 52);
+            this.cnbt.TabIndex = 0;
+            this.cnbt.Text = "ConnectDB";
+            this.cnbt.UseVisualStyleBackColor = true;
+            this.cnbt.Click += new System.EventHandler(this.Cnbt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 487);
+            this.Controls.Add(this.cnbt);
+            this.Font = new System.Drawing.Font("Phetsarath OT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cnbt;
+    }
+}
+
---
+++ SITDB/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SITDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Cnbt_Click(object sender, EventArgs e)
+        {
+            /* 
+             
+            string cnstring;
+            SqlConnection cnn;
+            cnstring = "Data Source=LAPTOP-MCN7P17D\\SQLEXPRESS;Initial Catalog=SITDB;Integrated Security=True;";
+            cnn = new SqlConnection(cnstring);
+            cnn.Open();
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+            sql = " SELECT * FROM SubjectTb";
+            command= new SqlCommand(sql, cnn);
+            dataReader= command.ExecuteReader();
+            while(dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + "_" + dataReader.GetValue(1) + "\n";
+            }
+            MessageBox.Show(Output);
+            dataReader.Close();
+            command.Dispose();
+            cnn.Close();
+            */
+
+            /*
+            string cnstring;
+            SqlConnection cnn;
+            cnstring = "Data Source=LAPTOP-MCN7P17D\\SQLEXPRESS;Initial Catalog=SITDB;Integrated Security=True;";
+            cnn = new SqlConnection(cnstring);
+            cnn.Open();
+            SqlCommand command;
+            SqlDataAdapter dataAdapter= new SqlDataAdapter();
+            String sql;
+            sql = "INSERT INTO SubjectTb(Subject) values('" + " visual.net " + "')";
+            command = new SqlCommand(sql, cnn);
+            dataAdapter.InsertCommand = new SqlCommand(sql, cnn);
+            dataAdapter.InsertCommand.ExecuteNonQuery();
+            command.Dispose();
+            MessageBox.Show("Data Inserted");
+            cnn.Close();
+            */
+
+            string cnstring;
+            SqlConnection cnn;
+            cnstring = "Data Source=LAPTOP-MCN7P17D\\SQLEXPRESS;Initial Catalog=SITDB;Integrated Security=True;";
+            cnn = new SqlConnection(cnstring);
+            cnn.Open();
+            SqlCommand command;
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            String sql;
+            sql = "UPDATE SubjectTb SET Subject='" + " Microsoft Studio Visual.net " + "' WHERE SubjectID=4";
+            command = new SqlCommand(sql, cnn);
+            dataAdapter.InsertCommand = new SqlCommand(sql, cnn);
+            dataAdapter.InsertCommand.ExecuteNonQuery();
+            command.Dispose();
+            MessageBox.Show("Data success Updated");
+            cnn.Close();
+
+        }
+    }
+}
